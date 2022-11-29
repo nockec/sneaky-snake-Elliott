0228766f6ac49fedbff15b46166938e30cd5ad6a
nockec <nockec@s.dcsdk12.org>
2022-11-29 18:21:29 -0500

    started game

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,41 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+let game = {
+	gridsize: 20,
+	refreshrate: 500, // in millisencondes
+};
+
+class player {
+	/**
+	 * @param {Number} x
+	 * @param {number} y
+	 */
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.w = game.gridsize;
+		this.y = this.w;
+		this.segments = [];
+	}
+}
+
+class segment {
+	/**
+	 * @param {Number} x
+	 * @param {number} y
+	 * @param {string} color
+	 */
+	constructor(x, y, color) {
+		this.x = x;
+		this.y = y;
+		this.w = game.gridsize;
+		this.y = this.w;
+		this.color = color;
+	}
+
+	update() {}
+
+	draw() {}
+}
